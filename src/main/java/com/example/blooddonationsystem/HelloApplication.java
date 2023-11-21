5026270e9d8d8fbfcade50fb79dfdf47eb8dd161
waqas2714 <waqasali00123@gmail.com>
2023-11-21 16:45:23 +0500

    HelloApplication for Tehreem's DB onnection

--- src/main/java/com/example/blooddonationsystem/HelloApplication.java
+++ src/main/java/com/example/blooddonationsystem/HelloApplication.java
@@ -6,42 +6,54 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class HelloApplication extends Application {
     private Connection connection;
 
     @Override
     public void start(Stage stage) throws IOException {
-//        establishDBConnection();
+        establishDBConnection();
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("removeDrive.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
 
-//        stage.setOnCloseRequest(event -> {
-//            closeDBConnection();
-//        });
+        stage.setOnCloseRequest(event -> {
+            closeDBConnection();
+        });
 
         stage.show();
     }
 
     private void establishDBConnection() {
-        String dbHost = "192.168.100.9";
-        String dbName = "blooddonationsystem";
-        String username = "dbsadmin";
-        String password = "dbssucks";
-        String url = "jdbc:mysql://" + dbHost + ":3306" + "/" + dbName;
+        String url = "jdbc:mysql://10.7.96.185:3306/sakila";
+//        String url = "jdbc:mysql://10.7.32.156:3306/blooddonationsystem";
+        String user = "Waqas";
+        String password = "MySqlPassword";
 
         try {
-            connection = DriverManager.getConnection(url, username, password);
+            // Establish the connection
+            Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the database!");
+//            Statement statement = conn.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM names");
+//
+//            // Iterate through the result set and create User objects
+//            while (resultSet.next()) {
+//                String name = resultSet.getString("name");
+//                System.out.println(name);
+//            }
+
+            // Close connections
+//            resultSet.close();
+//            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
     }
 
     private void closeDBConnection() {
