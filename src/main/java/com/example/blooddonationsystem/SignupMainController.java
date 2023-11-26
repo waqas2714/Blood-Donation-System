8ff79d182e106b6ff02eaac0c917ad60f4ee3d56
waqas2714 <waqasali00123@gmail.com>
2023-11-26 18:50:44 +0500

    Navigation bug handled in signupMain.fxml

--- src/main/java/com/example/blooddonationsystem/SignupMainController.java
+++ src/main/java/com/example/blooddonationsystem/SignupMainController.java
@@ -13,6 +13,8 @@ public class SignupMainController {
     private Button btnDonor;
     @FXML
     private Button btnOther;
+    @FXML
+    private Button btnGoToLogin;
 
     @FXML
     public void gotToDonor(ActionEvent e){
@@ -51,5 +53,25 @@ public class SignupMainController {
             ex.printStackTrace(); // Handle the exception appropriately
         }
     }
+
+    @FXML
+    private void GoToLogin(ActionEvent event) {
+        try {
+            // Load the login.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+
+            // Get the stage information
+            Stage stage = (Stage) btnGoToLogin.getScene().getWindow();
+            Scene scene = new Scene(root);
+
+            // Set the new scene onto the stage
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+    }
+
 
 }
