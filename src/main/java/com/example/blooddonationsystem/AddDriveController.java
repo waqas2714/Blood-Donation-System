228abff3663c040f2ec641adb2b7e9d884e0ea5b
AhmedSultan2002 <ahmedsultanx2002@gmail.com>
2023-12-17 22:56:00 +0500

    Scene swtiching between NGOmain and remove/add drive done

--- src/main/java/com/example/blooddonationsystem/AddDriveController.java
+++ src/main/java/com/example/blooddonationsystem/AddDriveController.java
@@ -1,4 +1,64 @@
 package com.example.blooddonationsystem;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.time.LocalDate;
+
 public class AddDriveController {
+
+    @FXML
+    private Button btnAddDrive;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private TextField txtFieldLoc;
+
+    @FXML
+    private DatePicker DateBox;
+
+
+    public void AddDriveFunc(ActionEvent event)
+    {
+        Connection con = HelloApplication.getConnection();
+        String Location = txtFieldLoc.getText();
+        LocalDate Date = DateBox.getValue();
+
+
+
+
+    }
+
+
+    public void GoBackToNGOMain(ActionEvent event)
+    {
+        // Load the ngoMain.fxml file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ngoMain.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Get the stage information
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        // Set the new scene onto the stage
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- src/main/java/com/example/blooddonationsystem/NgoMainController.java
+++ src/main/java/com/example/blooddonationsystem/NgoMainController.java
@@ -1,4 +1,65 @@
 package com.example.blooddonationsystem;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NgoMainController {
+
+
+    @FXML
+    private Button BtnAddDrive;
+
+    @FXML
+    private Button BtnRemoveDrive;
+
+
+    public void GoToAddDrive(ActionEvent event)
+    {
+        // Load the AddDrive.fxml file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addDrive.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Get the stage information
+        Stage stage = (Stage) BtnAddDrive.getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        // Set the new scene onto the stage
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void GoToRemoveDrive(ActionEvent event)
+    {
+        // Load the ngoMain.fxml file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("removeDrive.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Get the stage information
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //BtnRemoveDrive.getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        // Set the new scene onto the stage
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
